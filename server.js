4818002242c0b5003470ae82bc55a3a465d7c215
gowtham-ns <gowtham.ns@gmail.com>
2020-12-11 21:43:59 +0000

    Added Docker Jenkins

--- server.js
+++ server.js
@@ -11,19 +11,40 @@ const bodyParser = require('body-parser');
 // create the server
 const app = express();
 
-// the backend server will parse json, not a form request
-app.use(bodyParser.json());
+// bring in firestore
+const Firestore = require("@google-cloud/firestore");
 
-// mock events data - for a real solution this data should be coming 
-// from a cloud data store
-const mockEvents = {
-    events: [
-        { title: 'an event', id: 11, description: 'something really cool' },
-        { title: 'another event', id: 22, description: 'something even cooler' }
-    ]
+// initialize Firestore and set project id from env var
+const firestore = new Firestore(
+    {
+        projectId: process.env.GOOGLE_CLOUD_PROJECT
+    }
+);
+
+function getEvents(req, res) {
+    const returnObj = { events: []};
+        firestore.collection("Events").get()
+            .then((snapshot) => {
+                    if (!snapshot.empty) {
+                        snapshot.docs.forEach(doc => {
+                        const eventObj = doc.data();
+                        //get internal firestore id and assign to object
+                        eventObj.id = doc.id;
+                        //add object to array
+                        console.log(returnObj);
+                        returnObj.events.push(eventObj);
+                        }); 
+                }
+            res.json(returnObj);
+        })
+        .catch((err) => {
+            console.error('Error getting events', err);
+            res.json(returnObj);
+        });
 };
 
-
+// the backend server will parse json, not a form request
+app.use(bodyParser.json());
 
 
 // health endpoint - returns an empty array
@@ -40,7 +61,7 @@ app.get('/version', (req, res) => {
 // mock events endpoint. this would be replaced by a call to a datastore
 // if you went on to develop this as a real application.
 app.get('/events', (req, res) => {
-    res.json(mockEvents);
+    getEvents(req, res);
 });
 
 // Adds an event - in a real solution, this would insert into a cloud datastore.
@@ -48,15 +69,16 @@ app.get('/events', (req, res) => {
 // this will produce unexpected behavior in a stateless kubernetes cluster. 
 app.post('/event', (req, res) => {
     // create a new object from the json data and add an id
-    const ev = { 
+     const ev = { 
         title: req.body.title, 
-        description: req.body.description,
-        id : mockEvents.events.length + 1
+        description: req.body.description
      }
-    // add to the mock array
-    mockEvents.events.push(ev);
-    // return the complete array
-    res.json(mockEvents);
+
+    // this will create the Events collection if it does not exist
+    firestore.collection("Events").add(ev).then(ret => {
+        getEvents(req, res);
+    });
+
 });
 
 app.use((err, req, res, next) => {
